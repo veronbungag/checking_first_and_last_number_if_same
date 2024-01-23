6b6797ae75101bddb274219cc9168c10fe9acfff
veronbungag <rveron.bungag@gmail.com>
2024-01-23 23:36:05 +0800

    if first number is equal to last number

--- checking_first_and_last_number_if_same.py
+++ checking_first_and_last_number_if_same.py
@@ -1,8 +1,10 @@
 #define a function for  list of numbers
 def first_last_same(numberList):
-    first_num = numberList[0]
-    last_num = numberList[-1]
+    first_number = numberList[0]
+    last_number = numberList[-1]
 #if first number is equal to last number, return True
+    if first_number == last_number:
+        return True
 #if neither condition above is met, return False
 #given list set 1
 #print result for Set 1
