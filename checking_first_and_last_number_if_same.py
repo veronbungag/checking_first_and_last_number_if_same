71731426457b61046ff38bd6158baab75a7b8b83
veronbungag <rveron.bungag@gmail.com>
2024-01-23 23:30:49 +0800

    pseudocode

---
+++ checking_first_and_last_number_if_same.py
@@ -0,0 +1,7 @@
+#define a function for  list of numbers
+#if first number is equal to last number, return True
+#if neither condition above is met, return False
+#given list set 1
+#print result for Set 1
+#given list set 2
+#print result for Set 2
