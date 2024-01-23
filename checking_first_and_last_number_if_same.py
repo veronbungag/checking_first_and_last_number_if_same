94479c58630c1799e3f6c60355a26e73754d2795
veronbungag <rveron.bungag@gmail.com>
2024-01-23 23:38:42 +0800

    given list set 2

--- checking_first_and_last_number_if_same.py
+++ checking_first_and_last_number_if_same.py
@@ -13,4 +13,5 @@ numbers_set1 = [10, 20, 30, 40, 10]
 #print result for Set 1
 print("result is", first_last_same(numbers_set1))
 #given list set 2
+numbers_set2 = [75, 65, 35, 75, 30]
 #print result for Set 2
