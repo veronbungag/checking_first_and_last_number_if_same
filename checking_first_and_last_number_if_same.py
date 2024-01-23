343c523df5da066fbdd18186ad83a5f0da0efe74
veronbungag <rveron.bungag@gmail.com>
2024-01-23 23:34:39 +0800

    defining a function

--- checking_first_and_last_number_if_same.py
+++ checking_first_and_last_number_if_same.py
@@ -1,4 +1,7 @@
 #define a function for  list of numbers
+def first_last_same(numberList):
+    first_num = numberList[0]
+    last_num = numberList[-1]
 #if first number is equal to last number, return True
 #if neither condition above is met, return False
 #given list set 1
